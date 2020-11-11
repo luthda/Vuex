891beee5f793b7e1c77f2de39d38c8432fe008b2
luthda <david.luthiger@ost>
2020-11-11 14:50:50 +0100

    add vuex test component

---
+++ src/components/VuexComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+        <input type="number" v-model.number="incrementBy">
+    <button @click="incrementCount">Increment</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      incrementBy: 1
+    }
+  },
+  methods: {
+    incrementCount() {
+      this.$store.dispatch('updateIncrement', this.incrementBy)
+    }
+  }
+}
+</script>
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -4,8 +4,6 @@
     <p>This event is created by {{ user.name }}</p>
     <p>There are {{ catCount }} categories</p>
     <p>{{ getEvent(1) }}</p>
-    <input type="number" v-model.number="incrementBy">
-    <button @click="incrementCount">Increment</button>
   </div>
 </template>
 
@@ -14,22 +12,12 @@ import { mapState } from 'vuex'
 import { mapGetters } from 'vuex'
 
 export default {
-  data() {
-    return {
-      incrementBy: 1
-    }
-  },
   computed: {
     ...mapState(['categories', 'user']),
     ...mapGetters({
       catCount: 'categoriesLength',
       getEvent: 'getEventById'
     })
-  },
-  methods: {
-    incrementCount() {
-      this.$store.dispatch('updateIncrement', this.incrementBy)
-    }
   }
 }
 </script>
