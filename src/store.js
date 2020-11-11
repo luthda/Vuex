b44b47f173c06e865b306a9d8377a589c02476b4
luthda <david.luthiger@ost>
2020-11-11 14:35:50 +0100

    add getters & first mutation

--- src/store.js
+++ src/store.js
@@ -4,7 +4,43 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    count: 0,
+    user: { id: 123, name: 'Dave' },
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
+  mutations: {
+    INCREMENT_COUNT(state) {
+      state.count += 1
+    }
+  },
+  actions: {},
+  getters: {
+    categoriesLength: state => {
+      return state.categories.length
+    },
+    doneTodos: state => {
+      return state.todos.filter(todo => todo.done)
+    },
+    activeTodosCount: (state, getters) => {
+      return state.todos.length - getters.doneTodos.length
+    },
+    getEventById: state => id => {
+      return state.events.find(event => event.id == id)
+    }
+  }
 })
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,29 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event is created by {{ user.name }}</p>
+    <p>There are {{ catCount }} categories</p>
+    <p>{{ getEvent(1) }}</p>
+    <button @click="incrementCount">Increment</button>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['categories', 'user']),
+    ...mapGetters({
+      catCount: 'categoriesLength',
+      getEvent: 'getEventById'
+    })
+  },
+  methods: {
+    incrementCount() {
+      this.$store.commit('INCREMENT_COUNT')
+    }
+  }
+}
+</script>
